8f9c0dabf442c7deec822243eb65db47943baab2
Luis Corales <lrcorales@gmail.com>
2023-08-15 08:59:27 -0500

    modificaciones en el back

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const getGenres = require('./src/Controllers/getGenres.js')
+const getGenres = require('./src/controllers/getGenres.js')
 const port = process.env.PORT || 3001;
 
 // sincroniza todos los modelos junto con el servidor al iniciar.
