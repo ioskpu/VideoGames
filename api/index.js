1896a93482d746997afaa10b7fa63d99ff4f921f
ioskpu <lrcorales@gmail.com>
2023-10-09 07:47:28 -0500

    probando cambios para la vista de cards

--- api/index.js
+++ api/index.js
@@ -4,12 +4,13 @@ require("dotenv").config();
 // const axios = require("axios");
 const { loadGenres } = require("./src/controllers/genres.controllers");
 const { loadPlatforms } = require("./src/controllers/platforms.controllers");
+const PORT = process.env.PORT || 8080 || 5000 || 3000
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
   await loadGenres(true);
   await loadPlatforms(true);
   server.listen(process.env.PORT, () => {
-    console.log(`API server listening at ${process.env.PORT}`); // eslint-disable-line no-console
+    console.log(`API server listening at + ${PORT}`); // eslint-disable-line no-console
   });
 });
